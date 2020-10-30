323439c01845c7a455bc062e6104b9914a49f2ea
admin <rootaka@gmail.com>
2020-10-30 12:40:28 +0900

    build

---
+++ docs/precache-manifest.5422260d3b47001d48c106fb874b9529.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f60a9aa69816a1fdca630121329faac8",
+    "url": "/react-news-app/index.html"
+  },
+  {
+    "revision": "084e4473c528ae0fa8e4",
+    "url": "/react-news-app/static/css/main.9c3d03da.chunk.css"
+  },
+  {
+    "revision": "569ef69cf1b3b8650e2e",
+    "url": "/react-news-app/static/js/2.27cae3eb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-news-app/static/js/2.27cae3eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "084e4473c528ae0fa8e4",
+    "url": "/react-news-app/static/js/main.72565720.chunk.js"
+  },
+  {
+    "revision": "44f7c6019728532e2c3d",
+    "url": "/react-news-app/static/js/runtime-main.7cc599f8.js"
+  }
+]);
